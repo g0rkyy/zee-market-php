7927a30e3309b44aeec9742cfaa04d40847deb5d
gorkyy <lonelyfalkoor78@gmail.com>
2025-06-07 00:37:34 -0300

    not fuck yeah

--- includes/notifications.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sendDepositNotification($userId, $amount, $crypto) {
-    // Email/SMS quando depósito for confirmado
-}
-
-function sendOrderNotification($vendorId, $orderId) {
-    // Notificar vendedor sobre nova compra
-}
-?>
